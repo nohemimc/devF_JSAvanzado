3b67fcb08e4a6412bb3b2c26577e9971cbe489d7
Erika Miranda <nohemileonla@gmai.com>
2022-07-18 18:44:57 -0500

    Promesas: Promesa axios y promesa fetch

---
+++ promesas/promesaAxios.js
@@ -0,0 +1,32 @@
+"use strict"
+
+const axios = require("axios");
+const url = "https://rickandmortyapi.com/api/character/3";
+
+const promise = new Promise((resolve, reject) => {
+  axios
+    .get(url)
+    .then((response) => {
+      resolve(response.data);
+    })
+    .catch((error) => {
+      reject(error.response.data);
+    });
+});
+
+promise
+  .then((response) => {
+    console.log("response ", response);
+  })
+  .catch((error) => {
+    console.log("error ", error);
+  });
+
+//Forma corta
+// axios
+//     .get(url)
+//     .then((response) => {
+//         console.log('response', response.data);
+//     }).catch((error) => {
+//         console.log('error', error.data);
+//     })
--- promesas/promesaFetch.js
+++ promesas/promesaFetch.js
@@ -1,20 +1,68 @@
 "use strict";
 
-const url = "https://rickandmortyapi.com/api/character/3";
-
 // request => peticion
 // response => respuesta
+// Cuando una funcion no tiene un return retorna por defecto un undefined
+
+const url1 = "https://rickandmortyapi.com/api/character/3";
+const url2 = "https://pokeapi.co/api/v2/pokemon/pikachu";
+
 function getData(url) {
-  fetch(url)
-    .then((response) => response.json())
-    .then((response) => {
-      let nombre = document.getElementById("nombre");
-      nombre.innerHTML = `El nombre del personaje es ${response.name}`;
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log("error ", error);
-    });
+  return new Promise((resolve, reject) => {
+    fetch(url, { method: "GET" })
+      .then((response) => response.json())
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
 
-getData(url);
+const apiRickMorty = getData(url1);
+apiRickMorty
+  .then((response) => {
+    console.log("response ", response);
+  })
+  .catch((error) => {
+    console.log("error ", error);
+  });
+
+const pokemons = getData(url2);
+pokemons
+  .then((response) => {
+    console.log("response ", response);
+  })
+  .catch((error) => {
+    console.log("error ", error);
+  });
+
+// Para lanzar varias promesas en una se usa Promise.all
+// const pokemons = getData(url2);
+// const apiRickMorty = getData(url1);
+// const arreglo = [apiRickMorty, pokemons];
+// Promise.all(arreglo).then((arr) => {
+//   console.log(arr);
+// })
+
+//=========================================================================
+//Cuando una función no tiene un return retorna por defecto undefined
+//Se encierra "fetch" es una promesa, porque la función "getData" se ejecutaba, pero no aseguraba un resultado. Con la promesa, espera que se ejecute y lanza una respuesta. 
+// const url = "https://rickandmortyapi.com/api/character/3";
+// function getData(url) {
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((response) => {
+//       let nombre = document.getElementById("nombre");
+//       nombre.innerHTML = `El nombre del personaje es ${response.name}`;
+//       //console.log(response);
+//     })
+//     .catch((error) => {
+//       console.log("error ", error);
+//     });
+// }
+// getData(url);
+
+
+
